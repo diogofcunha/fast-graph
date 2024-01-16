1ebf7ba7e7f384602afc75dfe82f6a1d7fea66bc
Diogo Cunha <diogofncunha@gmail.com>
2024-01-16 23:12:52 +0000

    feat: 🎸 added dfs implementation

---
+++ src/__tests__/dfs.test.ts
@@ -0,0 +1,82 @@
+import { Node, Graph, SearchAlgorithmNodeBehavior } from "..";
+
+describe("DFS Implementation", () => {
+  test("DFS should visit all nodes in the correct order", () => {
+    const graph = new Graph<number>();
+
+    const nodeA = new Node<number>("A", 1);
+    const nodeB = new Node<number>("B", 2);
+    const nodeC = new Node<number>("C", 3);
+    const nodeD = new Node<number>("D", 4);
+    const nodeE = new Node<number>("E", 5);
+
+    graph.addNode(nodeA);
+    graph.addNode(nodeB);
+    graph.addNode(nodeC);
+    graph.addNode(nodeD);
+    graph.addNode(nodeE);
+
+    graph.addEdge(nodeA, nodeB);
+    graph.addEdge(nodeA, nodeC);
+    graph.addEdge(nodeB, nodeD);
+    graph.addEdge(nodeC, nodeD);
+    graph.addEdge(nodeD, nodeE);
+
+    const visitedOrder: Array<string> = [];
+    graph.dfs(node => {
+      visitedOrder.push(node.id);
+      return SearchAlgorithmNodeBehavior.continue;
+    });
+
+    const expectedOrder = [nodeA.id, nodeC.id, nodeD.id, nodeE.id, nodeB.id];
+
+    expect(visitedOrder).toEqual(expectedOrder);
+  });
+
+  test("DFS should stop when break is returned", () => {
+    const graph = new Graph<number>();
+
+    const nodeA = new Node<number>("A", 1);
+    const nodeB = new Node<number>("B", 2);
+    const nodeC = new Node<number>("C", 3);
+    const nodeD = new Node<number>("D", 4);
+    const nodeE = new Node<number>("E", 5);
+
+    graph.addNode(nodeA);
+    graph.addNode(nodeB);
+    graph.addNode(nodeC);
+    graph.addNode(nodeD);
+    graph.addNode(nodeE);
+
+    graph.addEdge(nodeA, nodeB);
+    graph.addEdge(nodeA, nodeC);
+    graph.addEdge(nodeB, nodeD);
+    graph.addEdge(nodeC, nodeD);
+    graph.addEdge(nodeD, nodeE);
+
+    const visitedOrder: Array<string> = [];
+    graph.dfs(node => {
+      visitedOrder.push(node.id);
+
+      return node.value === 3
+        ? SearchAlgorithmNodeBehavior.break
+        : SearchAlgorithmNodeBehavior.continue;
+    });
+
+    const expectedOrder = [nodeA.id, nodeC.id];
+
+    expect(visitedOrder).toEqual(expectedOrder);
+  });
+
+  test("DFS on an empty graph should do nothing", () => {
+    const graph = new Graph<number>();
+
+    const visitedOrder: Array<string> = [];
+    graph.dfs(node => {
+      visitedOrder.push(node.id);
+      return SearchAlgorithmNodeBehavior.continue;
+    });
+
+    expect(visitedOrder).toEqual([]);
+  });
+});
