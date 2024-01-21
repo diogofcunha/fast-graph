0c8ef7a1ff11b81eb32bcbc31e898e8ff4810217
Diogo Cunha <diogofncunha@gmail.com>
2024-01-21 22:18:47 +0000

    test: 💍 added coverage for dfs initial node

--- src/__tests__/dfs.test.ts
+++ src/__tests__/dfs.test.ts
@@ -33,6 +33,44 @@ describe("DFS Implementation unweighted graphs", () => {
     expect(visitedOrder).toEqual(expectedOrder);
   });
 
+  test("DFS should visit all nodes in the correct order", () => {
+    const graph = new Graph<number>();
+
+    const nodeA = new Node<number>("A", 1);
+    const nodeB = new Node<number>("B", 2);
+    const nodeC = new Node<number>("C", 3);
+    const nodeD = new Node<number>("D", 4);
+    const nodeE = new Node<number>("E", 5);
+    const nodeF = new Node<number>("F", 5);
+    const nodeG = new Node<number>("G", 5);
+
+    graph.addNode(nodeF);
+    graph.addNode(nodeG);
+    graph.addNode(nodeA);
+    graph.addNode(nodeB);
+    graph.addNode(nodeC);
+    graph.addNode(nodeD);
+    graph.addNode(nodeE);
+
+    graph.addEdge(nodeF, nodeG);
+    graph.addEdge(nodeG, nodeA);
+    graph.addEdge(nodeA, nodeB);
+    graph.addEdge(nodeA, nodeC);
+    graph.addEdge(nodeB, nodeD);
+    graph.addEdge(nodeC, nodeD);
+    graph.addEdge(nodeD, nodeE);
+
+    const visitedOrder: Array<string> = [];
+    graph.dfs(node => {
+      visitedOrder.push(node.id);
+      return SearchAlgorithmNodeBehavior.continue;
+    }, nodeA);
+
+    const expectedOrder = [nodeA.id, nodeC.id, nodeD.id, nodeE.id, nodeB.id];
+
+    expect(visitedOrder).toEqual(expectedOrder);
+  });
+
   test("DFS should stop when break is returned", () => {
     const graph = new Graph<number>();
 
@@ -110,6 +148,47 @@ describe("DFS Implementation weighted graphs", () => {
       costOrder.push(cost);
       return SearchAlgorithmNodeBehavior.continue;
     });
+
+    const expectedOrder = [nodeA.id, nodeC.id, nodeD.id, nodeE.id, nodeB.id];
+
+    expect(visitedOrder).toEqual(expectedOrder);
+    expect(costOrder).toEqual([0, 2, 4, 5, 1]);
+  });
+
+  test("DFS should visit all nodes in the correct order", () => {
+    const graph = new Graph<number>({ weighted: true });
+
+    const nodeA = new Node<number>("A", 1);
+    const nodeB = new Node<number>("B", 2);
+    const nodeC = new Node<number>("C", 3);
+    const nodeD = new Node<number>("D", 4);
+    const nodeE = new Node<number>("E", 5);
+    const nodeF = new Node<number>("F", 5);
+    const nodeG = new Node<number>("G", 5);
+
+    graph.addNode(nodeF);
+    graph.addNode(nodeG);
+    graph.addNode(nodeA);
+    graph.addNode(nodeB);
+    graph.addNode(nodeC);
+    graph.addNode(nodeD);
+    graph.addNode(nodeE);
+
+    graph.addEdge(nodeF, nodeG, 1);
+    graph.addEdge(nodeG, nodeA, 1);
+    graph.addEdge(nodeA, nodeB, 1);
+    graph.addEdge(nodeA, nodeC, 2);
+    graph.addEdge(nodeB, nodeD, 3);
+    graph.addEdge(nodeC, nodeD, 4);
+    graph.addEdge(nodeD, nodeE, 5);
+
+    const visitedOrder: Array<string> = [];
+    const costOrder: Array<number | undefined> = [];
+    graph.dfs((node, cost) => {
+      visitedOrder.push(node.id);
+      costOrder.push(cost);
+      return SearchAlgorithmNodeBehavior.continue;
+    }, nodeA);
 
     const expectedOrder = [nodeA.id, nodeC.id, nodeD.id, nodeE.id, nodeB.id];
 
