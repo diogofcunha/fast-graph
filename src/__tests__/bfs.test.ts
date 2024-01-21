87da493f1d4ceff1e288ee49514e2c450ee54e9c
Diogo Cunha <diogofncunha@gmail.com>
2024-01-21 17:52:41 +0000

    test: 💍 added coverage for bfs initial node

--- src/__tests__/bfs.test.ts
+++ src/__tests__/bfs.test.ts
@@ -122,6 +122,49 @@ describe("BFS Implementation weighted graph", () => {
     expect(costOrder).toEqual([0, 1, 2, 3, 5]);
   });
 
+  test("BFS should visit all connected nodes in the correct order if initial is supplied", () => {
+    const graph = new Graph<number>({ weighted: true });
+
+    const nodeF = new Node<number>("F", 5);
+    const nodeG = new Node<number>("G", 5);
+
+    const nodeA = new Node<number>("A", 1);
+    const nodeB = new Node<number>("B", 2);
+    const nodeC = new Node<number>("C", 3);
+    const nodeD = new Node<number>("D", 4);
+    const nodeE = new Node<number>("E", 5);
+
+    graph.addNode(nodeF);
+    graph.addNode(nodeG);
+    graph.addNode(nodeA);
+    graph.addNode(nodeB);
+    graph.addNode(nodeC);
+    graph.addNode(nodeD);
+    graph.addNode(nodeE);
+
+    graph.addEdge(nodeF, nodeG, 1);
+    graph.addEdge(nodeG, nodeA, 1);
+    graph.addEdge(nodeA, nodeB, 1);
+    graph.addEdge(nodeA, nodeC, 2);
+    graph.addEdge(nodeB, nodeD, 3);
+    graph.addEdge(nodeC, nodeD, 4);
+    graph.addEdge(nodeD, nodeE, 5);
+
+    const visitedOrder: Array<string> = [];
+    const costOrder: Array<number | undefined> = [];
+
+    graph.bfs((node, cost) => {
+      visitedOrder.push(node.id);
+      costOrder.push(cost);
+      return SearchAlgorithmNodeBehavior.continue;
+    }, nodeA);
+
+    const expectedOrder = [nodeA.id, nodeB.id, nodeC.id, nodeD.id, nodeE.id];
+
+    expect(visitedOrder).toEqual(expectedOrder);
+    expect(costOrder).toEqual([0, 1, 2, 3, 5]);
+  });
+
   test("BFS should stop when break is returned", () => {
     const graph = new Graph<number>({ weighted: true });
 
